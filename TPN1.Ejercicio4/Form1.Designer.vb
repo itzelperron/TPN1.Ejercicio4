f143abced6fabdb8641ff71faefba0932ce9c5ac
itzelperron@gmail.com <itzelperron@gmail.com>
2026-06-11 21:32:43 -0300

    Hola

--- TPN1.Ejercicio4/Form1.Designer.vb
+++ TPN1.Ejercicio4/Form1.Designer.vb
@@ -22,10 +22,42 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Button1 = New Button()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(239, 152)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(210, 101)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(225, 51)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(63, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Label1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Name = "Form1"
         Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
 
 End Class
